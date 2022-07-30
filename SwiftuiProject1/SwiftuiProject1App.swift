75e32aa1fe83da53ce6f09b28e712bf51f19d397
KenanSezginbas <31697908+Kenansezginbas@users.noreply.github.com>
2022-07-30 21:55:31 +0300

    Initial Commit

---
+++ SwiftuiProject1/SwiftuiProject1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiProject1App.swift
+//  SwiftuiProject1
+//
+//  Created by Kenan Developer on 30.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiProject1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
